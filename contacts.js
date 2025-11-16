046b2d5081b27cdda736fe8d163c7978872c9011
OlegKrivenko <krivenko.olegus@gmail.com>
2025-11-16 23:11:44 +0100

    add  function listContacts()

---
+++ contacts.js
@@ -0,0 +1,25 @@
+import fs from "fs/promises";
+import path from "path";
+
+const contactsPath = path.resolve("db", "contacts.json");
+
+async function listContacts() {
+  // ...твій код. Повертає масив контактів.
+
+  const buffer = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(buffer);
+}
+
+console.log(listContacts());
+
+async function getContactById(contactId) {
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту (з id).
+}
