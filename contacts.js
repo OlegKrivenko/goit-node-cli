17a7b49bd6466e4aa167813a58afeb4ae0d25180
OlegKrivenko <krivenko.olegus@gmail.com>
2025-11-17 23:28:51 +0100

    add getContactById(contactId)

--- contacts.js
+++ contacts.js
@@ -5,15 +5,16 @@ const contactsPath = path.resolve("db", "contacts.json");
 
 async function listContacts() {
   // ...твій код. Повертає масив контактів.
-
   const buffer = await fs.readFile(contactsPath, "utf-8");
   return JSON.parse(buffer);
 }
 
-console.log(listContacts());
-
 async function getContactById(contactId) {
   // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === contactId);
+  console.log(result);
+  return result || null;
 }
 
 async function removeContact(contactId) {
